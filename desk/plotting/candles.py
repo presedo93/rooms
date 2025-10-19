13c4acfdfb0670b93f0087252ada958932502b4c
Rene Presedo <presedo93@gmail.com>
2025-10-19 16:52:15 +0200

    feat(tape): improve candles plotting

--- desk/plotting/candles.py
+++ desk/plotting/candles.py
@@ -4,6 +4,13 @@ from decimal import Decimal
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
+INC_COLOR = "#3D9970"
+DEC_COLOR = "#FF4136"
+
+
+VOL_PARAMS = {"name": "Volume", "showlegend": False}
+OHLC_PARAMS = {"showlegend": False}
+
 
 def plot_candles(
     *,
@@ -14,18 +21,16 @@ def plot_candles(
     close: list[Decimal],
     volume: list[Decimal],
 ):
-    color = ["green" if c > o else "red" for c, o in zip(close, open)]
-
     fig = make_subplots(
         rows=2, cols=1, shared_xaxes=True, row_heights=[0.8, 0.2], vertical_spacing=0
     )
 
-    candlestick = go.Candlestick(x=x, open=open, high=high, low=low, close=close, showlegend=False)
-    volumes = go.Bar(x=x, y=volume, marker_color=color, name="Volume", showlegend=False)
+    color = [INC_COLOR if c > o else DEC_COLOR for c, o in zip(close, open)]
+    ohlc = go.Ohlc(x=x, open=open, high=high, low=low, close=close, **OHLC_PARAMS)
+    volumes = go.Bar(x=x, y=volume, marker_color=color, **VOL_PARAMS)
 
-    fig.add_trace(candlestick, 1, 1)
+    fig.add_trace(ohlc, 1, 1)
     fig.add_trace(volumes, 2, 1)
-
     fig.update_xaxes(rangeslider_visible=False)
 
     return fig
